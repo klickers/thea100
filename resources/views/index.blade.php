4b5ca79e6b76766d2e3115d158a49b205f46bb3c
klickers <bluebirid228@gmail.com>
2020-12-10 16:22:30 -0800

    set up index.blade.php and tailwind

---
+++ resources/views/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Exploring Theater</title>
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="/css/app.css" />
+    </head>
+    <body>
+        <!-- Navigation Sidebar -->
+        <nav class="side-nav">
+            <!-- Logo -->
+            <div id="logo">
+                <p>Exploring Theater</p>
+            </div>
+            <!-- Navigation Links -->
+            <div class="nav-links">
+                <ul>
+                    <a href="#!"><li>Plays</li></a>
+                    <a href="#!"><li>The Crew</li></a>
+                    <a href="#!"><li>The Cast</li></a>
+                </ul>
+            </div>
+        </nav>
+
+        <!-- Another Section -->
+    </body>
+</html>
